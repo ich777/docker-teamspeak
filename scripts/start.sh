35dc41b142a7c521cfa37df3c5a9a9d3cb8358a0
ich777 <ich777@noreply.example.org>
2020-05-14 10:04:13 +0200

    „scripts/start.sh“ ändern

--- scripts/start.sh
+++ scripts/start.sh
@@ -18,12 +18,10 @@ fi
 echo "---Starting...---"
 chown -R ${UID}:${GID} /opt/scripts
 chown -R ${UID}:${GID} ${DATA_DIR}
-killpid=0
+
 term_handler() {
-	if [ $killpid -ne 0 ]; then
-		kill -SIGTERM "$killpid"
-		wait "$killpid"
-	fi
+	kill -SIGTERM "$(pidof ts3server)"
+	tail --pid="(pidof ts3server)"
 	exit 143;
 }
 
